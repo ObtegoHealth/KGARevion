522589f6d0478040e822ca2d35e2b55c77de60f5
Josh Quintal <joshquin@gmail.com>
2025-06-26 08:19:04 +0100

    add offload capability for review model

--- action/inference_review.py
+++ action/inference_review.py
@@ -73,8 +73,8 @@ class ReviewInfer(object):
         return umls_to_ddb
     
     def load_model(self, model, model_weights):
-        model = PeftModel.from_pretrained(model, model_weights).cuda()
-        model.config.pad_token_id = self.tokenizer.eos_token_id 
+        model = PeftModel.from_pretrained(model, model_weights, device_map="auto", offload_folder="../offload").cuda()
+        model.config.pad_token_id = self.tokenizer.eos_token_id
         model = model.eval()
 
         return model
